174d2dbb534684d8e47def8e63468127b40ecbf5
jiuxia211 <2064166368@qq.com>
2025-02-05 20:37:50 +0800

    refactor: payment thrift (#1)
    * refactor: payment thrift
    
    * fix: thrift num error

--- idl/payment.thrift
+++ idl/payment.thrift
@@ -3,15 +3,39 @@ namespace go payment
 include "model.thrift"
 
 /*
+ * struct PaymentTokenRequest 支付令牌请求
+ * @Param orderID 商户订单号
+ * @Param userID 用户ID
+ */
+struct PaymentTokenRequest {
+    1: required i64 orderID
+    2: required i64 userID
+}
+
+/*
+ * struct PaymentTokenResponse 支付令牌响应
+ * @Param token 支付令牌
+ * @Param expirationTime 令牌过期时间
+ * @Param status 请求状态
+ */
+struct PaymentTokenResponse {
+    1: model.BaseResp base,
+    2: required string paymentToken
+    3: required i64 expirationTime
+}
+
+/*
  * struct PaymentRequest 订单结算请求
  * @Param orderID 商户订单号
  * @Param userID 用户ID
+ * @Param paymentToken 支付令牌
  * @Param creditCard 信用卡信息
  * @Param description 订单描述
  */
 struct PaymentRequest {
     1: required i64 orderID
     2: required i64 userID
+    3: required string paymentToken
     4: required model.CreditCardInfo creditCard
     5: optional string description
 }
