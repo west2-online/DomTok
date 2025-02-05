bb0b5d88955d6735e3e693438ce523e66362c882
jiuxia211 <2064166368@qq.com>
2025-02-05 13:57:56 +0800

    feat: empty user service

---
+++ internal/handler.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2024 The west2-online Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package internal
+
+import (
+	"context"
+
+	user "github.com/west2-online/DomTok/kitex_gen/user"
+)
+
+// UserServiceImpl implements the last service interface defined in the IDL.
+type UserServiceImpl struct{}
+
+// Register implements the UserServiceImpl interface.
+func (s *UserServiceImpl) Register(ctx context.Context, request *user.RegisterRequest) (resp *user.RegisterResponse, err error) {
+	// TODO: Your code here...
+	return
+}
+
+// Login implements the UserServiceImpl interface.
+func (s *UserServiceImpl) Login(ctx context.Context, request *user.LoginRequest) (resp *user.LoginResponse, err error) {
+	// TODO: Your code here...
+	return
+}
