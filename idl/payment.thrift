925e7237679af6ad3c30fa2157d81f0a0d9fd3c6
mutezebra <mutezebra@qq.com>
2025-02-06 15:49:10 +0800

    fix: fix idl

--- idl/payment.thrift
+++ idl/payment.thrift
@@ -39,3 +39,38 @@ struct PaymentRequest {
     4: required model.CreditCardInfo creditCard
     5: optional string description
 }
+
+struct PaymentResponse {
+    1: model.BaseResp base,
+    2: required i64 paymentID
+    3: required i64 status
+}
+
+/*
+ * struct RefundRequest 退款请求
+ * @Param orderID 关联的商户订单号
+ * @Param userID 用户ID
+ * @Param refundAmount 退款金额
+ * @Param refundReason 退款原因
+ */
+struct RefundRequest {
+    1: required i64 orderID
+    2: required i64 userID
+    3: required double refundAmount
+    4: required string refundReason
+}
+
+struct RefundResponse {
+    1: model.BaseResp base,
+    2: required i64 refundID
+    3: required i64 status
+}
+
+
+/*
+ * service PaymentService 支付服务
+ */
+service PaymentService {
+    PaymentResponse ProcessPayment(1: PaymentRequest request)
+    RefundResponse ProcessRefund(1: RefundRequest request)
+}
