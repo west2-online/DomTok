c948c992e0dc6f541d9e23f7994547841c5cc570
LingeringAutumn <1271354131@qq.com>
2025-02-11 20:18:47 +0800

    update:payment.thrift

--- idl/payment.thrift
+++ idl/payment.thrift
@@ -40,11 +40,37 @@ struct PaymentRequest {
     5: optional string description
 }
 
-
+/*
+ * struct PaymentResponse 订单支付响应
+ * @Param paymentID 支付ID
+ * @Param status 请求状态
+ */
 struct PaymentResponse {
     1: model.BaseResp base,
     2: required i64 paymentID
     3: required i64 status
+}
+
+/*
+ * struct RefundTokenRequest 退款令牌请求
+ * @Param orderID 商户订单号
+ * @Param userID 用户ID
+ */
+struct RefundTokenRequest {
+    1: required i64 orderID
+    2: required i64 userID
+}
+
+/*
+ * struct RefundTokenResponse 退款令牌响应
+ * @Param refundToken 退款令牌
+ * @Param expirationTime 令牌过期时间
+ * @Param status 请求状态
+ */
+struct RefundTokenResponse {
+    1: model.BaseResp base,
+    2: required string refundToken,
+    3: required i64 expirationTime
 }
 
 /*
@@ -61,6 +87,11 @@ struct RefundRequest {
     4: required string refundReason
 }
 
+/*
+ * struct RefundResponse 退款响应
+ * @Param refundID 退款ID
+ * @Param status 请求状态
+ */
 struct RefundResponse {
     1: model.BaseResp base,
     2: required i64 refundID
@@ -70,8 +101,14 @@ struct RefundResponse {
 
 /*
  * service PaymentService 支付服务
+ * @Method RequestPaymentToken 请求支付令牌
+ * @Method ProcessPayment 处理支付
+ * @Method RequestRefundToken 请求退款令牌
+ * @Method ProcessRefund 处理退款
  */
 service PaymentService {
     PaymentResponse ProcessPayment(1: PaymentRequest request)
+    PaymentTokenResponse RequestPaymentToken(1: PaymentTokenRequest request)
     RefundResponse ProcessRefund(1: RefundRequest request)
+    RefundTokenResponse RequestRefundToken(1: RefundTokenRequest request)
 }
