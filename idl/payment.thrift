16e8e9473961a7899b3780ec873dd54d774243b1
LingeringAutumn <1271354131@qq.com>
2025-02-11 17:38:25 +0800

    update:payment.thrift

--- idl/payment.thrift
+++ idl/payment.thrift
@@ -66,6 +66,89 @@ struct RefundResponse {
     3: required i64 status
 }
 
+/*
+ * struct PaymentOrder 支付订单
+ * @Param id 支付订单唯一标识
+ * @Param orderID 商户订单号
+ * @Param userID 用户ID(用户的唯一标识)
+ * @Param amount 订单总金额
+ * @Param status 支付状态：0-待支付，1-处理中，2-成功支付 3-支付失败
+ * @Param maskedCreditCardNumber 信用卡号国际信用卡号的最大长度为19 (仅存储掩码，如 **** **** **** 1234)
+ * @Param creditCardExpirationYear 信用卡到期年
+ * @Param creditCardExpirationMonth 信用卡到期月
+ * @Param description 订单描述
+ * @Param createdAt 订单创建时间
+ * @Param updatedAt 订单更新时间
+ * @Param deletedAt 订单删除时间
+ */
+struct PaymentOrder {
+    1: required i64 id
+    2: required i64 orderID
+    3: required i64 userID
+    4: required double amount
+    5: required i32 status
+    6: optional string maskedCreditCardNumber
+    7: optional i32 creditCardExpirationYear
+    8: optional i32 creditCardExpirationMonth
+    9: optional string description
+    10: optional string createdAt
+    11: optional string updatedAt
+    12: optional string deletedAt
+}
+
+/*
+ * struct RefundOrder 退款订单
+ * @Param id 支付退款唯一标识
+ * @Param orderID 关联的商户订单号
+ * @Param userID 用户ID(用户的唯一标识)
+ * @Param refundAmount 退款金额,单位为元
+ * @Param refundReason 退款原因
+ * @Param status 退款状态：0-待处理，1-处理中，2-成功退款 3-退款失败
+ * @Param maskedCreditCardNumber 信用卡号国际信用卡号的最大长度为19(仅存储掩码，如 **** **** **** 1234)
+ * @Param creditCardExpirationYear 信用卡到期年
+ * @Param creditCardExpirationMonth 信用卡到期月
+ * @Param createdAt 退款申请时间
+ * @Param updatedAt 退款最后更新时间
+ * @Param deletedAt 退款记录删除时间
+ */
+struct RefundOrder {
+    1: required i64 id
+    2: required string orderID
+    3: required i64 userID
+    4: required double refundAmount
+    5: optional string refundReason
+    6: required i32 status
+    7: optional string maskedCreditCardNumber
+    8: optional i32 creditCardExpirationYear
+    9: optional i32 creditCardExpirationMonth
+    10: optional string createdAt
+    11: optional string updatedAt
+    12: optional string deletedAt
+}
+
+/*
+ * struct PaymentLedger 支付流水
+ * @Param id 流水ID
+ * @Param referenceID 关联的支付订单或退款订单ID
+ * @Param userID 用户ID
+ * @Param amount 交易金额(正数表示收入，负数表示支出)
+ * @Param transactionType 交易类型：1-支付，2-退款，3-手续费，4-调整'
+ * @Param status 交易状态：0-待处理，1-成功，2-失败'
+ * @Param createdAt 交易创建时间
+ * @Param updatedAt 交易更新时间
+ * @Param deletedAt 交易记录删除时间
+ */
+struct PaymentLedger {
+    1: required i64 id
+    2: required i64 referenceID
+    3: required i64 userID
+    4: required double amount
+    5: required i32 transactionType
+    6: required i32 status
+    7: optional string createdAt
+    8: optional string updatedAt
+    9: optional string deletedAt
+}
 
 /*
  * service PaymentService 支付服务
