1b66d7b8daa3df56176780bfaaa0fd04048bb601
LingeringAutumn <1271354131@qq.com>
2025-02-14 01:42:17 +0800

    perfect interface

--- app/payment/controllers/rpc/handler.go
+++ app/payment/controllers/rpc/handler.go
@@ -10,6 +10,7 @@ type PaymentHandler struct {
 	useCase usecase.PaymentUseCase
 }
 
+// usecase只管逻辑，不涉及具体实现
 func (handler *PaymentHandler) ProcessPayment(ctx context.Context, req *payment.PaymentRequest) (r *payment.PaymentResponse, err error) {
 	r = new(payment.PaymentResponse)
 	p, err := handler.useCase.ProcessPayment(ctx, req.GetOrderID())
