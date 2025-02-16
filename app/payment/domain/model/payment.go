0dc74ca24b1f7527120a9c1dd0bbcb58329657a1
LingeringAutumn <1271354131@qq.com>
2025-02-16 19:38:58 +0800

    fix:payment/domain/model 把所有的int和int8改成

--- app/payment/domain/model/payment.go
+++ app/payment/domain/model/payment.go
@@ -17,10 +17,7 @@ limitations under the License.
 package model
 
 import (
-	"time"
-
 	"github.com/shopspring/decimal"
-	"gorm.io/gorm"
 )
 
 // PaymentOrder 支付订单表
@@ -29,14 +26,11 @@ type PaymentOrder struct {
 	OrderID                   int64           `gorm:"not null;comment:商户订单号"`
 	UserID                    int64           `gorm:"not null;comment:用户的唯一标识"`
 	Amount                    decimal.Decimal `gorm:"type:decimal(14,4);not null;comment:订单总金额"`
-	Status                    int8            `gorm:"not null;default:0;comment:支付状态：0-待支付，1-处理中，2-成功支付，3-支付失败"`
+	Status                    int64           `gorm:"not null;default:0;comment:支付状态：0-待支付，1-处理中，2-成功支付，3-支付失败"`
 	MaskedCreditCardNumber    string          `gorm:"size:19;comment:信用卡号（仅存储掩码，如 **** **** **** 1234）"`
-	CreditCardExpirationYear  int             `gorm:"comment:信用卡到期年"`
-	CreditCardExpirationMonth int             `gorm:"comment:信用卡到期月"`
+	CreditCardExpirationYear  int64           `gorm:"comment:信用卡到期年"`
+	CreditCardExpirationMonth int64           `gorm:"comment:信用卡到期月"`
 	Description               string          `gorm:"size:255;comment:订单描述信息"`
-	CreatedAt                 time.Time       `gorm:"not null;default:CURRENT_TIMESTAMP;comment:订单创建时间"`
-	UpdatedAt                 time.Time       `gorm:"not null;default:CURRENT_TIMESTAMP;autoUpdateTime;comment:订单最后更新时间"`
-	DeletedAt                 gorm.DeletedAt  `gorm:"index;comment:订单删除时间"`
 }
 
 // PaymentRefund 退款表
@@ -46,13 +40,10 @@ type PaymentRefund struct {
 	UserID                    int64           `gorm:"not null;comment:用户的唯一标识"`
 	RefundAmount              decimal.Decimal `gorm:"type:decimal(15,4);not null;comment:退款金额，单位为元"`
 	RefundReason              string          `gorm:"size:255;comment:退款原因"`
-	Status                    int8            `gorm:"not null;default:0;comment:退款状态：0-待处理，1-处理中，2-成功退款，3-退款失败"`
+	Status                    int64           `gorm:"not null;default:0;comment:退款状态：0-待处理，1-处理中，2-成功退款，3-退款失败"`
 	MaskedCreditCardNumber    string          `gorm:"size:19;comment:信用卡号（仅存储掩码，如 **** **** **** 1234）"`
-	CreditCardExpirationYear  int             `gorm:"comment:信用卡到期年"`
-	CreditCardExpirationMonth int             `gorm:"comment:信用卡到期月"`
-	CreatedAt                 time.Time       `gorm:"not null;default:CURRENT_TIMESTAMP;comment:退款申请时间"`
-	UpdatedAt                 time.Time       `gorm:"not null;default:CURRENT_TIMESTAMP;autoUpdateTime;comment:退款最后更新时间"`
-	DeletedAt                 gorm.DeletedAt  `gorm:"index;comment:退款记录删除时间"`
+	CreditCardExpirationYear  int64           `gorm:"comment:信用卡到期年"`
+	CreditCardExpirationMonth int64           `gorm:"comment:信用卡到期月"`
 }
 
 // PaymentLedger 流水信息表
@@ -62,11 +53,8 @@ type PaymentLedger struct {
 	UserID                    int64           `gorm:"not null;comment:用户的唯一标识"`
 	RefundAmount              decimal.Decimal `gorm:"type:decimal(15,4);not null;comment:退款金额，单位为元"`
 	RefundReason              string          `gorm:"size:255;comment:退款原因"`
-	Status                    int8            `gorm:"not null;default:0;comment:退款状态：0-待处理，1-处理中，2-成功退款，3-退款失败"`
+	Status                    int64           `gorm:"not null;default:0;comment:退款状态：0-待处理，1-处理中，2-成功退款，3-退款失败"`
 	MaskedCreditCardNumber    string          `gorm:"size:19;comment:信用卡号（仅存储掩码，如 **** **** **** 1234）"`
-	CreditCardExpirationYear  int             `gorm:"comment:信用卡到期年"`
-	CreditCardExpirationMonth int             `gorm:"comment:信用卡到期月"`
-	CreatedAt                 time.Time       `gorm:"not null;default:CURRENT_TIMESTAMP;comment:退款申请时间"`
-	UpdatedAt                 time.Time       `gorm:"not null;default:CURRENT_TIMESTAMP;autoUpdateTime;comment:退款最后更新时间"`
-	DeletedAt                 gorm.DeletedAt  `gorm:"index;comment:退款记录删除时间"`
+	CreditCardExpirationYear  int64           `gorm:"comment:信用卡到期年"`
+	CreditCardExpirationMonth int64           `gorm:"comment:信用卡到期月"`
 }
