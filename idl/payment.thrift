72f5d0cf207ac0fe6b53076820cc0c08a9d95edc
LingeringAutumn <1271354131@qq.com>
2025-02-19 20:33:41 +0800

    fix:payment.idl

--- idl/payment.thrift
+++ idl/payment.thrift
@@ -1,7 +1,27 @@
 namespace go payment
 
 include "model.thrift"
+/*
+ * struct PaymentTokenRequest 支付令牌请求
+ * @Param orderID 商户订单号
+ * @Param userID 用户ID
+ */
+struct PaymentTokenRequest {
+    1: required i64 orderID
+    2: required i64 userID
+}
 
+/*
+ * struct PaymentTokenResponse 支付令牌响应
+ * @Param token 支付令牌
+ * @Param expirationTime 令牌过期时间
+ * @Param status 请求状态
+ */
+struct PaymentTokenResponse {
+    1: model.BaseResp base,
+    2: required string paymentToken
+    3: required i64 expirationTime
+}
 /*
  * struct PaymentRequest 订单结算请求
  * @Param orderID 商户订单号
