add31732583b484602f4f7358bc33210204111b1
LingeringAutumn <1271354131@qq.com>
2025-02-28 01:04:07 +0800

    fix:lint 修正了import的格式错误

--- app/payment/usecase/service_test.go
+++ app/payment/usecase/service_test.go
@@ -1,17 +1,33 @@
+/*
+Copyright 2024 The west2-online Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package usecase
 
 import (
 	ctx "context"
 	"errors"
-	"github.com/west2-online/DomTok/app/payment/infrastructure/mysql"
-	"gorm.io/gorm"
 	"testing"
 
 	"github.com/bytedance/mockey"
 	"github.com/smartystreets/goconvey/convey"
+	"gorm.io/gorm"
 
 	"github.com/west2-online/DomTok/app/payment/domain/model"
 	"github.com/west2-online/DomTok/app/payment/domain/service"
+	"github.com/west2-online/DomTok/app/payment/infrastructure/mysql"
 	paymentStatus "github.com/west2-online/DomTok/pkg/constants"
 )
 
