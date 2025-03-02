7c8a089968f418189a59f5ca0ec51a979988dc35
jsbjkskbs <2478096487@qq.com>
2025-03-02 12:23:28 +0800

    fix: fix error handling in order status and payment methods

--- app/payment/infrastructure/rpc/rpc.go
+++ app/payment/infrastructure/rpc/rpc.go
@@ -49,11 +49,8 @@ func (rpc *paymentRPC) PaymentIsOrderExist(ctx context.Context, orderID int64) (
 
 func (rpc *paymentRPC) GetOrderStatus(ctx context.Context, orderID int64) (exist bool, expire int64, err error) {
 	resp, err := rpc.order.IsOrderExist(ctx, &orderrpc.IsOrderExistReq{OrderID: orderID})
-	if err != nil {
-		return false, -1, fmt.Errorf("rpc.order.IsOrderExist: %w", err)
-	}
-	if !utils.IsSuccess(resp.Base) {
-		return false, -1, fmt.Errorf("rpc.order.IsOrderExist: %v", resp.Base.Msg)
+	if err = utils.ProcessRpcError("rpc.order.IsOrderExist", resp, err); err != nil {
+		return false, 0, err
 	}
 	return resp.Exist, resp.OrderExpire, nil
 }
@@ -66,11 +63,8 @@ func (rpc *paymentRPC) OrderPaymentCancel(ctx context.Context, orderID int64, pa
 		PaymentStyle:  paymentStyle,
 	}
 	resp, err := rpc.order.OrderPaymentCancel(ctx, req)
-	if err != nil {
-		return fmt.Errorf("rpc.order.CancelOrder: %w", err)
-	}
-	if !utils.IsSuccess(resp.Base) {
-		return fmt.Errorf("rpc.order.CancelOrder: %v", resp.Base.Msg)
+	if err = utils.ProcessRpcError("rpc.order.OrderPaymentCancel", resp, err); err != nil {
+		return err
 	}
 	return nil
 }
@@ -83,11 +77,8 @@ func (rpc *paymentRPC) OrderPaymentSuccess(ctx context.Context, orderID int64, p
 		PaymentStyle:  paymentStyle,
 	}
 	resp, err := rpc.order.OrderPaymentSuccess(ctx, req)
-	if err != nil {
-		return fmt.Errorf("rpc.order.ConfirmOrder: %w", err)
-	}
-	if !utils.IsSuccess(resp.Base) {
-		return fmt.Errorf("rpc.order.ConfirmOrder: %v", resp.Base.Msg)
+	if err = utils.ProcessRpcError("rpc.order.OrderPaymentSuccess", resp, err); err != nil {
+		return err
 	}
 	return nil
 }
