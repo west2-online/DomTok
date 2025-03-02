d9c1f74cf163beee602933a09351faf4fa6f7f7c
FantasyRL <1845983502@qq.com>
2025-03-02 21:41:33 +0800

    merge main into cart

--- app/cart/usecase/cart_test.go
+++ app/cart/usecase/cart_test.go
@@ -1,10 +1,25 @@
+/*
+Copyright 2024 The west2-online Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package usecase
 
 import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/west2-online/DomTok/app/cart/infrastructure/rpc"
 	"testing"
 
 	"github.com/bytedance/mockey"
@@ -14,6 +29,7 @@ import (
 	"github.com/west2-online/DomTok/app/cart/domain/service"
 	"github.com/west2-online/DomTok/app/cart/infrastructure/db"
 	"github.com/west2-online/DomTok/app/cart/infrastructure/mq"
+	"github.com/west2-online/DomTok/app/cart/infrastructure/rpc"
 	metainfoContext "github.com/west2-online/DomTok/pkg/base/context"
 )
 
