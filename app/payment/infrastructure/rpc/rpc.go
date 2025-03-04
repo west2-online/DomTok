78120943e512733dcfb8ba1f611c57c18d0de9a5
LingeringAutumn <1271354131@qq.com>
2025-03-04 21:32:37 +0800

    fix:delete payment service_test.go

--- app/payment/infrastructure/rpc/rpc.go
+++ app/payment/infrastructure/rpc/rpc.go
@@ -39,12 +39,6 @@ func (rpc *paymentRPC) PaymentIsOrderExist(ctx context.Context, orderID int64) (
 		OrderID: orderID,
 	}
 	resp, err := rpc.order.IsOrderExist(ctx, orderRpcReq)
-	/*if err != nil {
-		return false, fmt.Errorf("rpc.order.IsOrderExist: %w", err)
-	}
-	if !utils.IsSuccess(resp.Base) {
-		return false, fmt.Errorf("rpc.order.IsOrderExist: %v", resp.Base.Msg)
-	}*/
 	if err = utils.ProcessRpcError("payment.IsOrderExist", resp, err); err != nil {
 		return false, err
 	}
